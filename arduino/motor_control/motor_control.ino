f5c6f1d8b0cccd0173c1d536b322094e4b14fd75
Mikaël Labrut <labrut@gmail.com>
2017-08-19 12:01:49 +0200

    wip

---
+++ arduino/motor_control/motor_control.ino
@@ -0,0 +1,182 @@
+/*
+  Sample Code to run the Sparkfun TB6612FNG 1A Dual Motor Driver using Arduino UNO R3
+
+  This code conducts a few simple manoeuvres to illustrate the functions:
+  - motorDrive(motorNumber, motorDirection, motorSpeed)
+  - motorBrake(motorNumber)
+  - motorStop(motorNumber)
+  - motorsStandby
+
+  Connections:
+  - Pin 3 ---> PWMA
+  - Pin 8 ---> AIN2
+  - Pin 9 ---> AIN1
+  - Pin 10 ---> STBY
+  - Pin 11 ---> BIN1
+  - Pin 12 ---> BIN2
+  - Pin 5 ---> PWMB
+
+  - Motor 1: A01 and A02
+  - Motor 2: B01 and B02
+
+*/
+
+//Define the Pins
+
+//Motor 1
+int pinAIN1 = 9; //Direction
+int pinAIN2 = 8; //Direction
+int pinPWMA = 3; //Speed
+
+//Motor 2
+int pinBIN1 = 11; //Direction
+int pinBIN2 = 12; //Direction
+int pinPWMB = 5; //Speed
+
+//Standby
+int pinSTBY = 10;
+
+//Constants to help remember the parameters
+static boolean turnCW = 0;  //for motorDrive function
+static boolean turnCCW = 1; //for motorDrive function
+static boolean motor1 = 0;  //for motorDrive, motorStop, motorBrake functions
+static boolean motor2 = 1;  //for motorDrive, motorStop, motorBrake functions
+static int veryLowSpeed = 150;
+static int lowSpeed = 90;
+static int normalSpeed = 125;
+
+void setup()
+{
+  //Set the PIN Modes
+  pinMode(pinPWMA, OUTPUT);
+  pinMode(pinAIN1, OUTPUT);
+  pinMode(pinAIN2, OUTPUT);
+
+  pinMode(pinPWMB, OUTPUT);
+  pinMode(pinBIN1, OUTPUT);
+  pinMode(pinBIN2, OUTPUT);
+
+  pinMode(pinSTBY, OUTPUT);
+
+  //updateShiftRegister(); 
+  Serial.begin(9600);
+
+  while (! Serial); // Wait until Serial is ready - Leonardo
+  Serial.println("Start !");
+}
+
+void loop()
+{
+
+  motorDrive(motor1, turnCW, veryLowSpeed);
+  Serial.println("low speed");
+  delay(5000);
+
+  motorStop(motor1);
+  Serial.println("stop");
+  delay(5000);
+
+  motorDrive(motor1, turnCCW, veryLowSpeed);
+  Serial.println("low speed reverse");
+  delay(5000);
+
+  motorStop(motor1);
+  Serial.println("stop");
+  delay(5000);
+
+  motorDrive(motor1, turnCW, veryLowSpeed);
+  Serial.println("low speed");
+  delay(5000);
+
+  motorsStandby();
+  Serial.println("stand by");
+  delay(5000);
+
+  motorDrive(motor1, turnCW, veryLowSpeed);
+  Serial.println("low speed");
+  delay(5000);
+
+  motorBrake(motor1);
+  Serial.println("brake");
+  delay(5000);
+}
+
+void motorDrive(boolean motorNumber, boolean motorDirection, int motorSpeed)
+{
+  /*
+    This Drives a specified motor, in a specific direction, at a specified speed:
+    - motorNumber: motor1 or motor2 ---> Motor 1 or Motor 2
+    - motorDirection: turnCW or turnCCW ---> clockwise or counter-clockwise
+    - motorSpeed: 0 to 255 ---> 0 = stop / 255 = fast
+  */
+
+  boolean pinIn1;  //Relates to AIN1 or BIN1 (depending on the motor number specified)
+
+
+  //Specify the Direction to turn the motor
+  //Clockwise: AIN1/BIN1 = HIGH and AIN2/BIN2 = LOW
+  //Counter-Clockwise: AIN1/BIN1 = LOW and AIN2/BIN2 = HIGH
+  if (motorDirection == turnCW)
+    pinIn1 = HIGH;
+  else
+    pinIn1 = LOW;
+
+  //Select the motor to turn, and set the direction and the speed
+  if (motorNumber == motor1)
+  {
+    digitalWrite(pinAIN1, pinIn1);
+    digitalWrite(pinAIN2, !pinIn1);  //This is the opposite of the AIN1
+    analogWrite(pinPWMA, motorSpeed);
+  }
+  else
+  {
+    digitalWrite(pinBIN1, pinIn1);
+    digitalWrite(pinBIN2, !pinIn1);  //This is the opposite of the BIN1
+    analogWrite(pinPWMB, motorSpeed);
+  }
+
+
+
+  //Finally , make sure STBY is disabled - pull it HIGH
+  digitalWrite(pinSTBY, HIGH);
+
+}
+
+void motorBrake(boolean motorNumber)
+{
+  /*
+    This "Short Brake"s the specified motor, by setting speed to zero
+  */
+
+  if (motorNumber == motor1)
+    analogWrite(pinPWMA, 0);
+  else
+    analogWrite(pinPWMB, 0);
+
+}
+
+
+void motorStop(boolean motorNumber)
+{
+  /*
+    This stops the specified motor by setting both IN pins to LOW
+  */
+  if (motorNumber == motor1) {
+    digitalWrite(pinAIN1, LOW);
+    digitalWrite(pinAIN2, LOW);
+  }
+  else
+  {
+    digitalWrite(pinBIN1, LOW);
+    digitalWrite(pinBIN2, LOW);
+  }
+}
+
+
+void motorsStandby()
+{
+  /*
+    This puts the motors into Standby Mode
+  */
+  digitalWrite(pinSTBY, LOW);
+}
